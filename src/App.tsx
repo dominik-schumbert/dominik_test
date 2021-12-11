4da2fb25ba0ed98b98af0e59d4ad1d961d51e660
dominik-schumbert <64913353+dominik-schumbert@users.noreply.github.com>
2021-12-11 15:43:14 +0100

    Changed the initial app

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let liste: Array<string> = ["Das", "ist", "eine", "Liste"];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {liste.map((element) => <li>{element}</li>)}
+        </ul>
       </header>
     </div>
   );
