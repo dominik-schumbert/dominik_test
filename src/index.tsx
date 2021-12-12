6ba40cdd04ca86f556e51f8c1d011253b86d3b96
dominik-schumbert <64913353+dominik-schumbert@users.noreply.github.com>
2021-12-12 12:35:44 +0100

    Another refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ToDoListApp from './ToDoApp';
-import MySql from './MySql';
+import ToDoListApp from './ToDo/ToDoApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
