de56430d1a8f78af426cd55d23ccd8c396702556
dominik-schumbert <64913353+dominik-schumbert@users.noreply.github.com>
2022-01-15 19:57:40 +0100

    Output in a pretty format

--- src/APITest.tsx
+++ src/APITest.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 
 export default function ApiTest() {
-  const [data, setData] = React.useState({});
   const [counter, setCounter] = React.useState(1);
+  const [artikel, setArtikel] = React.useState({});
 
   const fetchOnline = async () => {
     const response = await fetch("http://localhost:8000/artikel/" + counter.toString());
     const data = await response.json();
-    setData(data);
     setCounter(counter+1);
+    setArtikel((data as Array<any>)[0]);
   };
+
 
   return (
     <div>
-      <p>currently data = </p>
-      <pre>{JSON.stringify(data, null, "  ")}</pre>
+      <h2>New API data</h2>
+      <table className="table">
+      {
+        Object.entries(artikel).map(([key, value]) =>(
+          <tr>
+            <th>{key}</th>
+            <td>{(value as string)}</td>
+          </tr>
+        ))
+      }
+      </table>
       <button onClick={fetchOnline}>Fetch Data Online (async/await)</button>
     </div>
   );
